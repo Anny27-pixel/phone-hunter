548bc824ad24df30dddf5c626502578ac4a984ff
Anny27-pixel <jabintasmin102523@gmail.com>
2022-09-02 00:22:45 +0600

    added sensor features

--- js/app.js
+++ js/app.js
@@ -103,6 +103,7 @@ const displayPhoneDetails = phone => {
     <p>Release Date : ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'} </p>
     <p>Storage : ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Storage Information Found'}
     <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}   </p>
+    <p>Sensors: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : ' No Sensors'}  </p>
     
     
     
---
+++ js/summary.js
@@ -0,0 +1,38 @@
+/*
+FETCH
+1.fetch must provide url
+dynamic or static 
+2.how to convert fetch promise return to json
+3. how to convert json to data
+4. [cool headed]:
+-->array of object
+--->object with property
+--->object with a property caled data
+
+
+nested object 
+
+
+--------------------
+DOM manipulation
+1. get something from the DOM
+2. create element to append to the DOM
+3. dynamically load data based on id 
+
+
+
+---------------------
+
+array ---> forEach, map, find, filter
+
+---------------------------------
+template string
+1. dynamic innerHtml
+
+
+
+
+
+
+
+*/
